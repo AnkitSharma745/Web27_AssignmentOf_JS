90101267f6dc71cbbc8b2ee4bd00c5165a0ebba6
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-03 19:04:42 +0530

    Assignment 4 has been done

---
+++ JavaScript-VI-While-Loops-WE/problem1.js
@@ -0,0 +1,7 @@
+// Problem 1:
+// Print the numbers from the given starting point till ending point (including both start and end)
+
+let end = 13
+for (let i = 1; i <= end; i++) {
+  console.log(i)
+}

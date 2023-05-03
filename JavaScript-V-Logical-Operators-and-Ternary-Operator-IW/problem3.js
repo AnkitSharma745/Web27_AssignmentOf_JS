5209a8be8a531fd594dc15bf21188c85a874a873
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-03 18:59:57 +0530

    Assignment 3 has been done

---
+++ JavaScript-V-Logical-Operators-and-Ternary-Operator-IW/problem3.js
@@ -0,0 +1,7 @@
+// PROBLEM 3
+// Problem 3 : Given and character if it is a consonant print "Consonant"
+
+let char = "q"
+if (char !== "a" || char !== "e" || char !== "i" || char !== "o" || char !== "u") {
+  console.log("Consonant")
+}

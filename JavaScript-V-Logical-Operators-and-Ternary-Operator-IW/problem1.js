d474382937fea5e20b14f2d4ea04fcf85036dca2
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-03 18:59:20 +0530

    Assignment 3 has been done

---
+++ JavaScript-V-Logical-Operators-and-Ternary-Operator-IW/problem1.js
@@ -0,0 +1,12 @@
+// Problem 1: Given the year of birth, if the age is between 13 and 19 (both included) print Teenage and in between 20 and 29 (both included) print Twenties
+let birth=25
+if(birth>=13 && birth<=19){
+  console.log("Teenage")
+}
+else if(birth>=20 && birth<=29){
+  console.log("Twenties")
+}
+
+
+
+

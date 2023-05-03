6c3583f6e4b85b265ffe2bf9b3e1ab9230fe923b
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-03 18:53:00 +0530

    Assignment done

---
+++ Introduction-to-JS-Variables-and-Data-Types-Day-1 (1)/problem4.js
@@ -0,0 +1,15 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+let name = "Ankit Sharma"
+let school = "Masai"
+let grade = "I"
+let section = "A"
+let rollno = 34
+console.log("Student Name-", name)
+console.log("School Name-", school)
+console.log("Grade-", grade)
+console.log("Section-", section)
+console.log("Roll No.-", rollno)
+let physics = 90, chemistry = 89, math = 99
+
+console.log("Subject- Physics Chemistry Math")
+console.log("Marks-  ", physics, "    ", chemistry, "       ", math)

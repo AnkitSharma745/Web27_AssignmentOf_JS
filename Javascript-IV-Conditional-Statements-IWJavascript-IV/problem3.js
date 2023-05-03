935b2bdeabe293a415a10e8e7565692857a59085
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-03 18:56:31 +0530

    Assignment 2 has been done

---
+++ Javascript-IV-Conditional-Statements-IWJavascript-IV/problem3.js
@@ -0,0 +1,12 @@
+// Problem 3: Given 2 numbers a and b print which is greater or "both equal".
+let a=4
+let b=2
+if(a==b){
+  console.log("Both equal")
+}
+else if(a>b){
+  console.log("a is greater")
+}
+else{
+  console.log("b is greater ")
+}

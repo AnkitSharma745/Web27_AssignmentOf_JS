7d583ab0d90baeaa5a95d40913b3c7fe83654b00
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-04 23:13:47 +0530

    Arrays has been done

---
+++ JavaScript-VIII-Arrays-IW (1)/problem1.js
@@ -0,0 +1,5 @@
+// Problem 1 : Given an array print the position (starting with 1) and the element in a single line.
+let arr = [1, 2, 3, 4, 5, 6, 6]
+for (let i = 0; i < arr.length; i++) {
+  console.log("Indexing", i, " and element of the array", arr[i])
+}

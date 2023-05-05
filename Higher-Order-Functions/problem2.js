7e98c75dd5d0254c1d8ba7aa4987090d5eb7fa78
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-05 23:35:17 +0530

    Higher order function assignment has been done

---
+++ Higher-Order-Functions/problem2.js
@@ -0,0 +1,6 @@
+// Given an array of numbers generate an array containing the double value of the numbers
+let arr = [12, 3, 4, 5, 6]
+let a = arr.map(function(x) {
+  return x * 2
+})
+console.log(a)
